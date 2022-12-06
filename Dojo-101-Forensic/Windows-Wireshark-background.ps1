57efd35210a8c00ede192db5a4a994a0f82985a9
Taisen.fr (Dev) <vaca@hotmail.fr>
2022-12-06 11:34:48 +0100

    guid

--- Dojo-101-Forensic/Windows-Wireshark-background.ps1
+++ Dojo-101-Forensic/Windows-Wireshark-background.ps1
@@ -2,6 +2,6 @@
 
 stop-Process -name tshark
 cd 'C:\Program Files\Wireshark'
-$name = [string]$(Get-Random -Minimum 100 -Maximum 999999999999)
+$name = (new-guid).ToString()
 $name = "c:\test\${name}.pcap"
 .\tshark.exe -i "Wi-fi" -w $name
--- Dojo-101-SecOps/openssl.md
+++ Dojo-101-SecOps/openssl.md
@@ -3,4 +3,3 @@
 ### lire un certificat
 
 openssl x509 -in certificate.crt -text -noout
-
